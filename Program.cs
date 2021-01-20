7ac35435f69fb1dac9751b1ac4ac995d11c35dbc
Alyssa <aly.falshaw23@gmail.com>
2021-01-19 17:41:06 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace multiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //With the values 1-10, use code to generate a multiplication table
+            int[,] mTable = new int[10, 10];
+            
+            for (int i = 0; i < mTable.GetLength(0); i++)
+            {
+                for (int j = 0; j < mTable.GetLength(1); j++)
+                {
+                    mTable[i, j] = (i + 1) * (j + 1);
+                    Console.Write("{0,4}", mTable[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
